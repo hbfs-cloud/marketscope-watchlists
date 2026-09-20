139d6f31739a6b723970fbe1773a3cdc087ff05d
Mohamed El Ouadi <mohamed.elouadi@hbfs-cloud.com>
2026-09-20 21:40:14 +0200

    Refresh offline cache after responsive update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'marketscope-v2';
+const CACHE = 'marketscope-v3';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./data/watchlists.json','./manifest.webmanifest','./icon.svg','./og-card.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
